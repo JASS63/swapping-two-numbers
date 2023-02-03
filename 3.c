d41599bcce8479060945c96afe54eec410483dca
JASS63 <124114911+JASS63@users.noreply.github.com>
2023-02-03 00:43:36 -0800

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{  
+   int first, second,temp;
+   printf("Enter the first number: ");
+   scanf("%d",&first);
+   printf("Enter the second number:");
+   scanf("%d",&second);
+   
+   temp = first;
+   first=second;
+   second=temp;
+   
+   printf("\nAfter swapping, first number= %d\n", first);
+   printf("After swapping, second number =%d", second);
+   return 0; 
+   
+}
